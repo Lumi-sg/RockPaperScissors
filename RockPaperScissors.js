8c1cf91733bd3daecc4a04ec529de76c315b994a
Lumi <shaicloud@gmail.com>
2022-09-30 03:58:15 -0400

    first functional rps build

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,13 +1,34 @@
-let options = [
-    "Rock",
-    "Paper",
-    "Scissors"
-];
-
-function getComputerChoice()
-{
-    let computerChoice = options[Math.floor(Math.random()*options.length)];
-    return computerChoice;
+function getPlayerChoice() {
+  let playerInput = prompt("Type Rock, Paper, or Scissors");
+  console.log("Player picks: " + playerInput.toLowerCase());
+  return playerInput.toLowerCase();
 }
 
-console.log(getComputerChoice());
+function getComputerChoice() {
+  let options = ["Rock", "Paper", "Scissors"];
+  let computerChoice = options[Math.floor(Math.random() * options.length)];
+  console.log("Computer picks: " + computerChoice.toLowerCase());
+  return computerChoice.toLowerCase();
+}
+
+function checkWinner(playerInput, computerChoice) {
+  if (computerChoice === playerInput) {
+    return "Tie";
+  } else if (
+    (playerInput === "rock" && computerChoice == "scissors") ||
+    (playerInput === "paper" && computerChoice == "rock") ||
+    (playerInput === "scissors" && computerChoice == "paper")
+  ) {
+    return "Player Wins";
+  } else {
+    return "Computer Wins";
+  }
+}
+
+function playRPS() {
+  let playerChoice = getPlayerChoice();
+  let computerChoice = getComputerChoice();
+  console.log(checkWinner(playerChoice, computerChoice));
+}
+
+playRPS();
